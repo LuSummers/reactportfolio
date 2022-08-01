7b959d83ec8fa3eb37a020ba30ae54fe58ae434b
Lu Summers <lusummers@users.noreply.github.com>
2022-08-01 17:11:42 -0400

    updated home and about me

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+// import { HashRouter, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import About from './components/About'
+
+function App (){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Homepage</h1>
+      <Home></Home>
+      <About></About>
     </div>
-  );
+  )
 }
 
 export default App;
--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 // import Portrait from "../../assets/images/profile_pic.jpg";
 
 function Home() {
   return (
     <section className="home my-5">
       <div className="title">
-        <h2>Hi, I'm Kara</h2>
-        <p>Welcome to my Student Portfolio!</p>
-        <Link to="portfolio">
+        <h2>Hi, I'm Lulu Summers</h2>
+        <p>lorem!</p>
+        {/* <Link to="portfolio">
           <button>View my Projects</button>
-        </Link>
+        </Link> */}
       </div>
       <div className="portrait">
         {/* <img
